0cdcda9e9136b21e836fc5c8c59adc683bee03aa
Andrew Beers <andybeers@gmail.com>
2016-10-18 13:45:08 -0700

    Greet method will generate random name if greet is passed a name and another arg

--- greet.js
+++ greet.js
@@ -1,3 +1,5 @@
+var generate = require('project-name-generator');
+
 function greet(name) {
   if (name === 'Andy') {
     return 'Heyyyy, what\'s up, buddy?';
@@ -7,9 +9,12 @@ function greet(name) {
   }
 }
 
-if (process.argv.length > 2) {
+if (process.argv.length === 3) {
   var bashName = greet(process.argv[2]);
   console.log(bashName);
+} else if (process.argv.length === 4) {
+  var bashName = greet(process.argv[2]);
+  console.log(bashName + '! Your new computer alias is: ' + generate().spaced); 
 }
 
 module.exports = greet;
