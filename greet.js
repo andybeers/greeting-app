b44ae7de7e3ac70ee8a700ecb4e111d09cb924aa
Andrew Beers <andybeers@gmail.com>
2016-10-18 09:49:56 -0700

    changing indentation

--- greet.js
+++ greet.js
@@ -1,15 +1,15 @@
 function greet(name) {
-    if (name === "Andy") {
-        return "Heyyyy, what's up, buddy?";
+  if (name === "Andy") {
+      return "Heyyyy, what's up, buddy?";
     } else {
-        name = name || 'friend';
-        return 'hello ' + name;
+      name = name || 'friend';
+      return 'hello ' + name;
     }
 }
 
 if (process.argv.length > 2) {
-    var bashName = greet(process.argv[2]);
-    console.log(bashName);
+  var bashName = greet(process.argv[2]);
+  console.log(bashName);
 }
 
 module.exports = greet;
